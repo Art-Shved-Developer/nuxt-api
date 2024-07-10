7ef6f14c1db0b7ba28904f0946fe0ae15cf404a3
Artyom Bashkatov <ar.bashkatov@saychas.ru>
2024-07-10 11:15:51 +0300

    Change url to json mock data at production environment

--- server/api/pokemon.ts
+++ server/api/pokemon.ts
@@ -3,7 +3,10 @@ import { readFile } from 'fs/promises';
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
 
-  const module = await readFile(`public/${query.name}.json`, 'utf-8');
+  const url = process.env.NODE_ENV === 'production' ? `/${query.name}.json` : `public/${query.name}.json`
+  console.log('Debug: ', process)
+
+  const module = await readFile(url, 'utf-8');
     return {
       pokemon: JSON.parse(module)
     }
